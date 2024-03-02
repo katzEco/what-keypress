34b30b12e64a0921c36e562d98ed3936530cddd5
Suphakit P <me@suphakit.net>
2024-03-03 06:58:22 +0700

    feat<components>: footer

---
+++ src/lib/Components/footer.svelte
@@ -0,0 +1,3 @@
+<div class="w-full py-4 fixed bottom-0 text-center bg-whitesmoke text-jet">
+&copy; 2023 <a href="https://suphakit.net" target="_blank" class="hover:opacity-60 hover:underline">Suphakit P.</a> All Right Reserved.
+</div>
