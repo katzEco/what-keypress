9b93e248732bdfb86bfedc8944889dda46e7c3b5
Suphakit P <me@suphakit.net>
2024-03-03 07:56:34 +0700

    feat<layout>: setting up layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import '../app.css'
+
+  import Header from '$lib/Components/navbar.svelte';
+  import Footer from '$lib/Components/footer.svelte';
+</script>
+
+<div class="text-whitesmoke w-full">
+	<Header />
+
+	<main class="min-h-screen w-full bg-jet text-whitesmoke py-16">
+		<slot />
+	</main>
+
+	<Footer />
+</div>
+
+<style>
+	
+</style>
